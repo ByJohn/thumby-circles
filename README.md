2a79c75460947085603739b23e9ab7610946edae
John Evans <john.evans9103@gmail.com>
2023-04-07 19:53:46 +0100

    Reduce iterations by drawing circles in eighths

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Thumby Circles
 Circle MicroPython library for the [Thumby](https://thumby.us/) games console.
 
-## Method
-As a compromise between render speed and file size, the circles are not computed mathematically per se. Instead, pixel drawing instructions are manually pre-calculated for a quarter circle of each diameter. The pixels are draw with four-way symmetry to complete the circle.
-
 ## Usage
 
 ```python
 import circles
 
-# Displays a white (1), hollow circle with a diameter of 12 centred at position 36,20.
-drawCircle(36, 20, 12, 1)
+# Displays a hollow circle with a diameter of 12 centred at position 36,20.
+circles.drawCircle(36, 20, 12)
 ```
+
+## Method
+As a compromise between render speed and file size, the circles are not computed mathematically per se. Instead, pixel drawing instructions are manually pre-calculated for an eighth of a circle of each diameter. The pixels are draw with eight-way symmetry to complete the circle. It draws a circle in a similar way to the [midpoint circle algorithm](https://en.wikipedia.org/wiki/Midpoint_circle_algorithm) except the points are pre-calculated and the flat sides are drawn all at once.
+
 ## Supported Circle Sizes
 By default, circles with a diameter between 1-25 are supported. These are defined in the `circleSizes` object. You can add you own sizes or remove ones that you do not need (to save space).
--- circles.py
+++ circles.py
@@ -1,30 +1,32 @@
+# https://github.com/ByJohn/thumby-circles
+
 import thumby
 import math
 
-# Supported circles diameters (including 1, 2 and 3)
+# Supported circles diameters (including 1-3)
 circleSizes = {
     4: [1],
     5: [1],
     6: [1],
-    7: [2, 1],
-    8: [2, 1],
-    9: [2, 1],
-    10: [3, 1, 1],
-    11: [3, 2, 1],
-    12: [4, 2, 1, 1],
-    13: [4, 2, 1, 1],
-    14: [4, 3, 2, 1],
-    15: [5, 3, 2, 1, 1],
-    16: [5, 3, 2, 1, 1],
-    17: [6, 4, 2, 2, 1, 1],
-    18: [6, 4, 3, 2, 1, 1],
-    19: [6, 4, 3, 2, 1, 1],
-    20: [7, 5, 3, 2, 2, 1, 1],
-    21: [7, 5, 4, 3, 2, 1, 1],
-    22: [8, 5, 4, 3, 2, 1, 1, 1],
-    23: [8, 6, 4, 3, 2, 2, 1, 1],
-    24: [9, 6, 5, 4, 3, 2, 1, 1, 1],
-    25: [9, 7, 5, 4, 3, 2, 2, 1, 1],
+    7: [2],
+    8: [2],
+    9: [2],
+    10: [3, 1],
+    11: [3, 2],
+    12: [4, 2],
+    13: [4, 2],
+    14: [4, 3],
+    15: [5, 3],
+    16: [5, 3],
+    17: [6, 4, 2],
+    18: [6, 4, 3],
+    19: [6, 4, 3],
+    20: [7, 5, 3],
+    21: [7, 5, 4],
+    22: [8, 5, 4],
+    23: [8, 6, 4],
+    24: [9, 6, 5, 4],
+    25: [9, 7, 5, 4],
 }
 
 def drawCircle(x, y, diameter, colour = 1):
@@ -48,26 +50,45 @@ def drawCircle(x, y, diameter, colour = 1):
 
     prevInset2 = circleSizes[diameter][0]
 
+    # inset1 = loop iteration, inset2 = pixel offset
     for inset1, inset2 in enumerate(circleSizes[diameter]):
-        extra = prevInset2 - 1 - inset2 # Extra length possibly needed for this line
-
-        # If the current inset2 has reduced by more than 1
-        if extra > 0:
-            thumby.display.drawLine(left + inset1, top + inset2, left + inset1, top + inset2 + extra, colour) # Top left
-            thumby.display.drawLine(right - inset2, top + inset1, right - inset2 - extra, top + inset1, colour) # Top right
-            thumby.display.drawLine(right - inset1, bottom - inset2, right - inset1, bottom - inset2 - extra, colour) # Bottom right
-            thumby.display.drawLine(left + inset2, bottom - inset1, left + inset2 + extra, bottom - inset1, colour) # Bottom left
+        if inset1 == 0:
+            # Straight sides
+            thumby.display.drawLine(left + inset2, top, right - inset2, top, colour) # Top
+            thumby.display.drawLine(right, top + inset2, right, bottom - inset2, colour) # Right
+            thumby.display.drawLine(right - inset2, bottom, left + inset2, bottom, colour) # Bottom
+            thumby.display.drawLine(left, bottom - inset2, left, top + inset2, colour) # Left
         else:
-            thumby.display.setPixel(left + inset1, top + inset2, colour) # Top left
-            thumby.display.setPixel(right - inset2, top + inset1, colour) # Top right
-            thumby.display.setPixel(right - inset1, bottom - inset2, colour) # Bottom right
-            thumby.display.setPixel(left + inset2, bottom - inset1, colour) # Bottom left
+            extra = prevInset2 - 1 - inset2 # Extra length possibly needed for this line
+
+            # If the current inset2 has reduced by more than 1
+            if extra > 0:
+                # Draw lines
+                thumby.display.drawLine(left + inset1, top + inset2, left + inset1, top + inset2 + extra, colour) # WNW
+                thumby.display.drawLine(left + inset2, top + inset1, left + inset2 + extra, top + inset1, colour) # NNW
+                thumby.display.drawLine(right - inset2, top + inset1, right - inset2 - extra, top + inset1, colour) # NNE
+                thumby.display.drawLine(right - inset1, top + inset2, right - inset1, top + inset2 + extra, colour) # ENE
+                thumby.display.drawLine(right - inset2, bottom - inset1, right - inset2 - extra, bottom - inset1, colour) # ESE
+                thumby.display.drawLine(right - inset1, bottom - inset2, right - inset1, bottom - inset2 - extra, colour) # SSE
+                thumby.display.drawLine(left + inset2, bottom - inset1, left + inset2 + extra, bottom - inset1, colour) # SSW
+                thumby.display.drawLine(left + inset1, bottom - inset2, left + inset1, bottom - inset2 - extra, colour) # WSW
+            else:
+                # Otherwise draw dots
+                thumby.display.setPixel(left + inset1, top + inset2, colour) # WNW
+                thumby.display.setPixel(left + inset2, top + inset1, colour) # NNW
+                thumby.display.setPixel(right - inset2, top + inset1, colour) # NNE
+                thumby.display.setPixel(right - inset1, top + inset2, colour) # ENE
+                thumby.display.setPixel(right - inset2, bottom - inset1, colour) # ESE
+                thumby.display.setPixel(right - inset1, bottom - inset2, colour) # SSE
+                thumby.display.setPixel(left + inset2, bottom - inset1, colour) # SSW
+                thumby.display.setPixel(left + inset1, bottom - inset2, colour) # WSW
 
         prevInset2 = inset2
 
-    inset1 = inset1 + 1 # Increment once more for the straight sides
-
-    thumby.display.drawLine(left + inset1, top, right - inset1, top, colour) # Top
-    thumby.display.drawLine(right, top + inset1, right, bottom - inset1, colour) # Right
-    thumby.display.drawLine(right - inset1, bottom, left + inset1, bottom, colour) # Bottom
-    thumby.display.drawLine(left, bottom - inset1, left, top + inset1, colour) # Left
+    # Possibly fill in the four corners
+    if prevInset2 > len(circleSizes[diameter]):
+        inset1 = inset1 + 1
+        thumby.display.setPixel(left + inset1, top + inset1, colour) # NW
+        thumby.display.setPixel(right - inset1, top + inset1, colour) # NE
+        thumby.display.setPixel(right - inset1, bottom - inset1, colour) # SE
+        thumby.display.setPixel(left + inset1, bottom - inset1, colour) # SW
